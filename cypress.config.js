d9bda0448ed3891d8f0c139cb3fa9baa63a01f0c
angelk <danielperman12@gmail.com>
2024-07-11 18:17:55 -0600

    Configuraciones

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
+const { configureAllureAdapterPlugins } = require("@mmisty/cypress-allure-adapter/plugins");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      configureAllureAdapterPlugins(on, config);
+      return config;
     },
   },
 });
