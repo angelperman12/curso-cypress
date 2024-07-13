4f91a728dad97a36c62552eede7dd9b357a931c3
angelk <danielperman12@gmail.com>
2024-07-12 18:09:34 -0600

    Login page

---
+++ cypress/e2e/pages/login/login.data.js
@@ -0,0 +1,8 @@
+export class LoginData{
+    static get invalidateCredentials(){
+        return {
+            username: "abc",
+            password: "abc",
+        };
+    }
+}
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,12 @@
+const { LoginMethods } = require("./pages/login/login.methods")
+
+
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://www.demoblaze.com/')
+    cy.get('a[data-target="#logInModal"]').click()
+    LoginMethods.login('user', 'pass')
+    cy.wait(10000)
   })
 })
