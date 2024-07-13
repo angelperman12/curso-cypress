815687d3c96f6f6ed032411e5a26ff0dea826e61
angelk <danielperman12@gmail.com>
2024-07-12 20:46:09 -0600

    Product  Details

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,13 +1,14 @@
 const { HomeMethods } = require("./pages/home/home.methods")
 const { LoginMethods } = require("./pages/login/login.methods")
+const { ProductDetailsMethods } = require("./pages/product-details/product-details.methods")
 const { SignupMethods } = require("./pages/signup/signup.methods")
 
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://www.demoblaze.com/')
+    cy.visit('https://www.demoblaze.com/prod.html?idp_=1')
     //cy.get('a[data-target="#signInModal"]').click()
     //SignupMethods.signup('user1', 'user1')
-    HomeMethods.clickOnProductLink()
+    ProductDetailsMethods.clickOnAddToCartButton()
 
     cy.wait(5000)
   })
