4f979642eecb7eff3b5ef3b466e81a4ea9da545c
angelk <danielperman12@gmail.com>
2024-07-12 22:54:17 -0600

    deshabilitar la generación de video

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 
 module.exports = defineConfig({
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       configureAllureAdapterPlugins(on, config);
