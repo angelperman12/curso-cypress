14e2087d242a1f5abd932e1dbe3c061512a31aaf
angelk <danielperman12@gmail.com>
2024-07-12 18:38:35 -0600

    Solución para problema con type

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,12 +1,10 @@
 const { LoginMethods } = require("./pages/login/login.methods")
 
-
-
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://www.demoblaze.com/')
     cy.get('a[data-target="#logInModal"]').click()
-    LoginMethods.login('user', 'pass')
+    LoginMethods.login('usermuylargo', 'passmuylargo')
     cy.wait(10000)
   })
 })
